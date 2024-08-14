740ebde28259f0a38ae386470ce0f3fd45cc394e
EsvinMax1994 <maxdani94@outlook.com>
2024-08-13 18:24:48 -0600

    se agrega versionamiento de java

---
+++ Ejemplo1/src/main/java/com/mycompany/ejemplo1/ejemplo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author Maximus
+ */
+public class ejemplo {
+    
+    //psv ctrl+space
+    public static void main(String[] args) {
+        //sout ctrl+space
+        System.out.println("Hola Mundo");
+        
+    }
+}
